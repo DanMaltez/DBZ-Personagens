462ef5183e58c92b4697d96b1b9059fd3b226118
Dan Maltez <dan.maltezz@gmail.com>
2024-10-25 11:13:57 -0300

    Add files via upload

---
+++ Dragon Ball Z/js/script.js
@@ -0,0 +1,65 @@
+const logo = document.querySelector('#logo_dbz')
+const androide_18 = document.querySelector('#androide_18')
+const vegeta = document.querySelector('#vegeta')
+const majin_boo = document.querySelector('#majin_boo')
+const freeza = document.querySelector('#freeza')
+const trunks = document.querySelector('#trunks')
+const cell = document.querySelector('#cell')
+const piccolo = document.querySelector('#picolo')
+const kuririn = document.querySelector('#kuririn')
+const shenlong = document.querySelector('#shenlong')
+const mr_satan = document.querySelector('#mr_satan')
+
+
+androide_18.addEventListener('click',foto_androide_18)
+vegeta.addEventListener('click', foto_vegeta)
+majin_boo.addEventListener('click', foto_majin_boo)
+freeza.addEventListener('click', foto_freeza)
+trunks.addEventListener('click', foto_trunks)
+cell.addEventListener('click', foto_cell)
+piccolo.addEventListener('click', foto_piccolo)
+kuririn.addEventListener('click' ,foto_kuririn)
+shenlong.addEventListener('click', foto_shenlong)
+mr_satan.addEventListener('click', foto_mrsatan)
+
+
+function foto_mrsatan(){
+    logo.src='images/satan.png'
+}
+
+function foto_androide_18(){
+    logo.src='images/androide18.png'
+}
+
+function foto_vegeta(){
+    logo.src='images/vegeta.png'
+}
+
+function foto_majin_boo(){
+    logo.src='images/majin_boo.png'
+}
+
+function foto_freeza(){
+    logo.src='images/freeza.png'
+}
+
+function foto_trunks(){
+    logo.src='images/trunks.png'
+}
+
+function foto_cell(){
+    logo.src='images/cell.png'
+}
+
+function foto_piccolo(){
+    logo.src='images/piccolo.png'
+}
+
+function foto_kuririn(){
+    logo.src='images/kuririn.png'
+}
+
+function foto_shenlong(){
+    logo.src='images/shenlong.png'
+}
+
